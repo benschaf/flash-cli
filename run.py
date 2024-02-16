44f0fc2f7ce9a5ddac1d61e786b734db52e4ab69
Benjamin Schäfer <benibschafer@gmail.com>
2024-02-16 09:04:50 +0000

    feat: add base functionality (enlarge for info)
    add Flashcard_Set class which holds a list of Flashcard objects
    
    add mastery_level attribute to Flashcard class
    
    add flashcard_mode function which allows user to go through flashcards
    one by one
    
    add pick_mode function which allows user to pick a quiz mode
    
    update main function to manage the new pick_mode()

--- run.py
+++ run.py
@@ -13,6 +13,21 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('flash_cli_sheet')
 
+class Flashcard_Set:
+    def __init__(self, title):
+        self.title = title
+        self.flashcards = self._load_flashcards()
+
+    def _load_flashcards(self):
+        flashcards = []
+        worksheet = SHEET.worksheet(self.title).get_all_records()
+        for row in worksheet:
+            question = row["question"]
+            answer = row["answer"]
+            mastery_level = row["mastery_level"]
+            flashcards.append(Flashcard(question, answer, mastery_level))
+
+        return flashcards
 class Flashcard:
     """
     A class to represent a flashcard.
@@ -20,3 +35,38 @@ class Flashcard:
     def __init__(self, question, answer, mastery_level):
         self.question = question
         self.answer = answer
+        self.mastery_level = mastery_level
+
+def flashcard_mode():
+    print("Flashcard mode\n")
+    print("Loading set...")
+    my_set = Flashcard_Set("flashcards")
+    for flashcard in my_set.flashcards:
+        flashcard.show_question()
+        input("Press any key to show the answer")
+        flashcard.show_answer()
+        answer = input("Did you know the answer? (y/n): ")
+        if answer == "y":
+            flashcard.update_mastery(1)
+        elif answer == "n":
+            flashcard.update_mastery(-1)
+        print("Next question:")
+    print("Lesson finished")
+    print("Uploading...")
+    my_set.upload()
+    print("Successfully uploaded.")
+
+def pick_mode():
+    print("f    Flashcard Mode")
+    print("t    Type answer Mode")
+    selected_mode = input("Select a mode: ")
+    return selected_mode
+    
+def main():
+    mode = pick_mode()
+    if mode == "f":
+        flashcard_mode()
+    elif mode == "t":
+        type_answer_mode()
+
+main()
